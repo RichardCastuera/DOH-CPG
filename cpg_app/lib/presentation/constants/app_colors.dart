98145c7de8716629b380944642c79549e6d77fe2
Richard <richardcastuera16@gmail.com>
2024-07-11 11:34:06 +0800

    chore(project): Added asset files

---
+++ cpg_app/lib/presentation/constants/app_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // * Non initable class
+  const AppColors._();
+
+  static const Color primaryColor = Color(0xFF5DAE8B);
+  static const Color secondaryColor = Color(0xFFF6F49D);
+
+  static const Color white = Color(0xFFF7F7F7);
+  static const Color black = Color(0xFF393E46);
+}
---
+++ cpg_app/lib/presentation/screens/home_screen.dart
@@ -0,0 +1,23 @@
+import 'package:cpg_app/presentation/constants/app_colors.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Home'),
+          backgroundColor: AppColors.primaryColor,
+        ),
+      ),
+    );
+  }
+}
