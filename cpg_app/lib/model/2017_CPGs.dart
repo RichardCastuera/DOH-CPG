cfb71fbe2235e3a33c56d30d19d7fe3d84aa41a2
Richard <richardcastuera16@gmail.com>
2024-07-11 12:58:06 +0800

    chore(model/2017_CPGs): Added 2017 CPGs data model

---
+++ cpg_app/lib/model/2017_CPGs.dart
@@ -0,0 +1,25 @@
+class CPG2017 {
+  final String cpgTitle;
+  final String leadDevelopers;
+
+  CPG2017({
+    required this.cpgTitle,
+    required this.leadDevelopers,
+  });
+}
+
+List<CPG2017> cpg2017 = [
+  CPG2017(
+    cpgTitle: 'Diagnosis and Management of Low Back Pain (2nd Edition)',
+    leadDevelopers: 'Philippine Academy of Rehabilitation Medicine',
+  ),
+  CPG2017(
+    cpgTitle: 'Stroke Rehabilitation (2nd Edition)',
+    leadDevelopers: 'Philippine Academy of Rehabilitation Medicine',
+  ),
+  CPG2017(
+    cpgTitle: 'Immunization for Women',
+    leadDevelopers:
+        'Philippine Obstetrical and Gynecological Society Foundation, Inc.',
+  ),
+];
